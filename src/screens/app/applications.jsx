9b6dc1b2ab151b817d6ec6ea4694b8ea2eb3cb9d
wide27joy <rogerantony40@gmail.com>
2024-06-12 04:11:02 +0530

    added loading screen for applications

--- src/screens/app/applications.jsx
+++ src/screens/app/applications.jsx
@@ -1,23 +1,20 @@
-import React, { useEffect,useState } from 'react';
-import { Grid, Container, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Grid, Container, Typography, Backdrop, CircularProgress } from '@mui/material';
 import ApplicationTile from '../../components/applicatiom_tile';
 import AppBarCus from '../../components/appbar_custom';
 import DrawerCus from '../../components/drawer_custom_freelancer';
 import { fetchMyApplications } from "../../api/posts";
 
-
-
 const JobApplicationsPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [applications, setApplications] = useState([]);
-  
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   useEffect(() => {
     async function fetchApplications() {
       const response = await fetchMyApplications();
       if (response.status) {
         setApplications(response.data);
-        console.log(response.data);
       } else {
         console.log("Error fetching posts:", response.message);
       }
@@ -25,38 +22,46 @@ const JobApplicationsPage = () => {
     }
     fetchApplications();
   }, []);
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const toggleMenu = () => {
-      setIsMenuOpen((prevState) => !prevState);
-    };
-  return (<>
-    <AppBarCus onMenuIconClick={toggleMenu} showMenuIcon />
-    <DrawerCus open={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
 
+  const toggleMenu = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
 
-    <div
-      style={{
-        backgroundColor: "#f0f0f0",
-        minHeight: "100vh",
-        padding: "20px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-    <Container>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Your Job Applications
-      </Typography>
-      <Grid container spacing={2}>
-        {applications.map((job, index) => (
-          <Grid item key={index}>
-            <ApplicationTile jobTitle={job.title} jobStatus={job.status} companyName={job.company} />
+  return (
+    <>
+      <AppBarCus onMenuIconClick={toggleMenu} showMenuIcon />
+      <DrawerCus open={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
+
+      <div
+        style={{
+          backgroundColor: "#f0f0f0",
+          minHeight: "100vh",
+          padding: "20px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Container>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Your Job Applications
+          </Typography>
+          <Grid container spacing={2}>
+            {isLoading ? (
+              <Backdrop open={isLoading}>
+                <CircularProgress color="inherit" />
+              </Backdrop>
+            ) : (
+              applications.map((job, index) => (
+                <Grid item key={index}>
+                  <ApplicationTile jobTitle={job.title} jobStatus={job.status} companyName={job.company} />
+                </Grid>
+              ))
+            )}
           </Grid>
-        ))}
-      </Grid>
-    </Container>
-    </div></>
+        </Container>
+      </div>
+    </>
   );
 };
 
